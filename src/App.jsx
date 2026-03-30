0a7ae99148e825bad49f6bdd8600adc84a4ebfdc
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-03-30 15:55:31 +0200

    create a base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layout/DefaultLayout"
 
 function App() {
 
 
   return (
-    <>
 
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DefaultLayout />}>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
+
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+
+    return (
+        <h1>
+            footer
+        </h1>
+    )
+
+}
