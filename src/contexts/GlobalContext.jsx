3d6d6a99de747bb79485fd47ee132f379e6031d7
gilli-jonathan <gilli.jonathan97@gmail.com>
2026-03-31 10:51:14 +0200

    start milestone 2

---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+function TaskProvider({ children }) {
+
+
+}
+
+function useTask() {
+
+}
+
+export { TaskProvider, useTask }
